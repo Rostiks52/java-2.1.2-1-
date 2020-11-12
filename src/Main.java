9813823a049738c76c219fa4311828afe3d34fe8
Rostiks52 <ksyu_05@mail.ru>
2020-11-12 23:12:58 +0300

    Added src

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int startbalance = 100;
+        int bonuspercent;
+        int refill = 1100;
+        if (refill >= 1000) {
+            bonuspercent = 1;
+        } else {
+            bonuspercent = 0;
+        }
+        int totalbalance = startbalance + refill + refill * bonuspercent / 100;
+        System.out.println(totalbalance);
+    }
+}
